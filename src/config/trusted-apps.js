50e79d00c633fb54a033df3b8dac1e1fc838d834
srosset81 <srosset81@gmail.com>
2024-03-19 00:11:47 +0100

    Links to v2 docs and Mastopod

--- src/config/trusted-apps.js
+++ src/config/trusted-apps.js
@@ -1,51 +1,54 @@
+import mastopod from '~/assets/images/mastopod.png';
+import welcomeToMyPlace from '~/assets/images/welcometomyplace.png';
+import mutualAid from '~/assets/images/mutual-aid.png';
+import yourApp from '~/assets/images/your-application.png';
+
 export default [
-	{
-		'@type': 'apods:TrustedApps',
-		'apods:name': 'Welcome to my place',
-		'apods:description':
-			'Private meetings at home that promote a living together based on welcome, trust and mutual aid.',
-		'apods:domainName': 'welcometomyplace.org',
-		'apods:handledTypes': 'https://www.w3.org/ns/activitystreams#Event',
-		'apods:locales': 'en',
-		'apods:logo': 'https://welcometomyplace.org/logo512.png',
-		'apods:sourceCode': 'https://github.com/assemblee-virtuelle/welcometomyplace',
-	},
-	{
-		'@type': 'apods:TrustedApps',
-		'apods:name': 'Mutual Aid',
-		'apods:description': 'Classified ads oriented around mutual aid and shareable within a trusted network.',
-		'apods:domainName': 'mutual-aid.app',
-		'apods:handledTypes': [
-			'http://virtual-assembly.org/ontologies/pair-mp#Request',
-			'http://virtual-assembly.org/ontologies/pair-mp#Offer',
-		],
-		'apods:locales': 'en',
-		'apods:logo': 'https://mutual-aid.app/logo512.png',
-		'apods:sourceCode': 'https://github.com/assemblee-virtuelle/mutual-aid.app',
-	},
-	{
-		'@type': 'apods:TrustedApps',
-		'apods:name': 'Bienvenue chez moi',
-		'apods:description':
-			'Rencontres privées à domicile qui favorisent un vivre ensemble basé sur l’accueil, la confiance et l’entraide.',
-		'apods:domainName': 'bienvenuechezmoi.org',
-		'apods:handledTypes': 'https://www.w3.org/ns/activitystreams#Event',
-		'apods:locales': 'fr',
-		'apods:logo': 'https://bienvenuechezmoi.org/logo512.png',
-		'apods:sourceCode': 'https://github.com/assemblee-virtuelle/welcometomyplace',
-	},
-	{
-		'@type': 'apods:TrustedApps',
-		'apods:name': "L'Entraide",
-		'apods:description':
-			"Petites annonces orientées autour de l'entraide et partageables au sein d'un réseau de confiance.",
-		'apods:domainName': 'lentraide.app',
-		'apods:handledTypes': [
-			'http://virtual-assembly.org/ontologies/pair-mp#Request',
-			'http://virtual-assembly.org/ontologies/pair-mp#Offer',
-		],
-		'apods:locales': 'fr',
-		'apods:logo': 'https://lentraide.app/logo512.png',
-		'apods:sourceCode': 'https://github.com/assemblee-virtuelle/mutual-aid.app',
-	},
+  {
+    '@type': 'apods:TrustedApps',
+    'apods:name': 'Mastopod',
+    'apods:description': 'A Mastodon-compatible app that saves all data in your Pod.',
+    'apods:domainName': 'mastopod.com',
+    'apods:handledTypes': 'https://www.w3.org/ns/activitystreams#Note',
+    'apods:locales': 'en',
+    'apods:logo': mastopod,
+    'apods:sourceCode': 'https://github.com/assemblee-virtuelle/mastopod',
+  },
+  {
+    '@type': 'apods:TrustedApps',
+    'apods:name': 'Welcome to my place',
+    'apods:description':
+      'Private meetings at home that promote a living together based on welcome, trust and mutual aid.',
+    'apods:domainName': 'welcometomyplace.org',
+    'apods:handledTypes': 'https://www.w3.org/ns/activitystreams#Event',
+    'apods:locales': 'en',
+    'apods:logo': welcomeToMyPlace,
+    'apods:sourceCode': 'https://github.com/assemblee-virtuelle/welcometomyplace',
+  },
+  {
+    '@type': 'apods:TrustedApps',
+    'apods:name': 'Mutual Aid',
+    'apods:description': 'Classified ads oriented around mutual aid and shareable within a trusted network.',
+    'apods:domainName': 'mutual-aid.app',
+    'apods:handledTypes': [
+      'http://virtual-assembly.org/ontologies/pair-mp#Request',
+      'http://virtual-assembly.org/ontologies/pair-mp#Offer',
+    ],
+    'apods:locales': 'en',
+    'apods:logo': mutualAid,
+    'apods:sourceCode': 'https://github.com/assemblee-virtuelle/mutual-aid.app',
+  },
+  {
+    '@type': 'apods:TrustedApps',
+    'apods:name': 'Your application ?',
+    'apods:description': 'Creating social apps is easy with the ActivitPods framework. Give it a try !',
+    'apods:domainName': 'docs.activitypods.org',
+    'apods:handledTypes': [
+      'http://virtual-assembly.org/ontologies/pair-mp#Request',
+      'http://virtual-assembly.org/ontologies/pair-mp#Offer',
+    ],
+    'apods:locales': 'en',
+    'apods:logo': yourApp,
+    'apods:sourceCode': 'https://github.com/assemblee-virtuelle/activitypods',
+  },
 ];
