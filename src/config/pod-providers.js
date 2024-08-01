0a32972b40d96931294b11d7ed4222c7282e62c4
srosset81 <srosset81@gmail.com>
2024-08-01 15:57:31 +0200

    Use baseUrl instead of domainName for pod providers

--- src/config/pod-providers.js
+++ src/config/pod-providers.js
@@ -3,22 +3,22 @@ export const slug = 'apods:domainName';
 export default [
   {
     type: 'apods:PodProvider',
+    'apods:baseUrl': 'https://bocage.me',
     'apods:area': 'Orne, France',
-    'apods:domainName': 'bocage.me',
     'apods:locales': 'fr',
-    'apods:providedBy': 'SyReeN',
+    'apods:providedBy': 'SyRéeN',
   },
   {
     type: 'apods:PodProvider',
+    'apods:baseUrl': 'https://armoise.co',
     'apods:area': 'Oise, France',
-    'apods:domainName': 'armoise.co',
     'apods:locales': 'fr',
     'apods:providedBy': 'Réseaux de Vie',
   },
   {
     type: 'apods:PodProvider',
+    'apods:baseUrl': 'https://mypod.store',
     'apods:area': 'France',
-    'apods:domainName': 'mypod.store',
     'apods:locales': 'en',
     'apods:providedBy': 'Assemblée Virtuelle',
   },
