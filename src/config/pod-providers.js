5114e1c860caedebc8c7c22db2b31cb222d3d362
srosset81 <srosset81@gmail.com>
2024-10-24 09:27:35 +0200

    Remove bocage.me provider (not compatible with v2)

--- src/config/pod-providers.js
+++ src/config/pod-providers.js
@@ -1,19 +1,11 @@
 export default [
   {
-    slug: 'bocage.me',
-    type: 'apods:PodProvider',
-    'apods:baseUrl': 'https://bocage.me',
-    'apods:area': 'Orne, France',
-    'apods:locales': 'fr',
-    'apods:providedBy': 'SyRéeN',
-  },
-  {
     slug: 'armoise.co',
     type: 'apods:PodProvider',
     'apods:baseUrl': 'https://armoise.co',
     'apods:area': 'Oise, France',
     'apods:locales': 'fr',
-    'apods:providedBy': 'Réseaux de Vie',
+    'apods:providedBy': 'Réseaux de Vie'
   },
   {
     slug: 'mypod.store',
@@ -21,6 +13,6 @@ export default [
     'apods:baseUrl': 'https://mypod.store',
     'apods:area': 'France',
     'apods:locales': 'en',
-    'apods:providedBy': 'Assemblée Virtuelle',
-  },
+    'apods:providedBy': 'Assemblée Virtuelle'
+  }
 ];
