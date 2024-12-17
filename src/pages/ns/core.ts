efbbddf2d9c4a22d79af862338df9c21b4e7786c
srosset81 <srosset81@gmail.com>
2024-12-17 12:23:14 +0100

    Simplify ontology display

--- src/pages/ns/core.ts
+++ src/pages/ns/core.ts
@@ -1,29 +1,14 @@
-import path from 'path';
-import { readFileSync } from 'fs';
 import type { APIRoute } from 'astro';
 
 export const prerender = false;
 
-export const GET: APIRoute = async ({ request }) => {
-  const currentPath = import.meta.url.replace('file:', '');
-  let filePath: string;
-  let contentType: string;
-
+export const GET: APIRoute = async ({ request, redirect }) => {
   switch (request.headers.get('Accept')) {
     case 'application/ld+json':
-      filePath = '../../../ontology/ontology.jsonld';
-      contentType = 'application/ld+json';
-      break;
+      return redirect('/ns/core.jsonld', 303);
 
     case 'text/turtle':
     default:
-      filePath = '../../../ontology/ontology.ttl';
-      contentType = 'text/turtle';
-      break;
+      return redirect('/ns/core.ttl', 303);
   }
-
-  const buffer = readFileSync(path.join(currentPath, filePath));
-  return new Response(buffer, {
-    headers: new Headers({ 'Content-Type': contentType })
-  });
 };
