d97619a9edc21ede8404316beab733d56aac75f1
srosset81 <srosset81@gmail.com>
2025-10-22 16:54:29 +0200

    Add trailing slashes to Pod providers

--- src/config/pod-providers.js
+++ src/config/pod-providers.js
@@ -2,7 +2,7 @@ export default [
   {
     slug: 'alwest.org',
     type: 'apods:PodProvider',
-    'apods:baseUrl': 'https://alwest.org',
+    'apods:baseUrl': 'https://alwest.org/',
     'apods:area': 'Ouest de la France',
     'apods:locales': 'fr',
     'apods:providedBy': 'Reconnexion'
@@ -10,7 +10,7 @@ export default [
   {
     slug: 'armoise.co',
     type: 'apods:PodProvider',
-    'apods:baseUrl': 'https://armoise.co',
+    'apods:baseUrl': 'https://armoise.co/',
     'apods:area': 'Oise, France',
     'apods:locales': 'fr',
     'apods:providedBy': 'Reconnexion'
@@ -19,7 +19,7 @@ export default [
   {
     slug: 'mypod.store',
     type: 'apods:PodProvider',
-    'apods:baseUrl': 'https://mypod.store',
+    'apods:baseUrl': 'https://mypod.store/',
     'apods:area': 'France',
     'apods:locales': 'en',
     'apods:providedBy': 'Reconnexion'
