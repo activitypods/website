ef591fff5f74f12370a615943c0f4d871a42a4c8
srosset81 <srosset81@gmail.com>
2025-10-22 16:41:13 +0200

    Add alwest.org Pod provider

--- src/config/pod-providers.js
+++ src/config/pod-providers.js
@@ -1,18 +1,27 @@
 export default [
   {
+    slug: 'alwest.org',
+    type: 'apods:PodProvider',
+    'apods:baseUrl': 'https://alwest.org',
+    'apods:area': 'Ouest de la France',
+    'apods:locales': 'fr',
+    'apods:providedBy': 'Reconnexion'
+  },
+  {
     slug: 'armoise.co',
     type: 'apods:PodProvider',
     'apods:baseUrl': 'https://armoise.co',
     'apods:area': 'Oise, France',
     'apods:locales': 'fr',
-    'apods:providedBy': 'Réseaux de Vie'
+    'apods:providedBy': 'Reconnexion'
   },
+
   {
     slug: 'mypod.store',
     type: 'apods:PodProvider',
     'apods:baseUrl': 'https://mypod.store',
     'apods:area': 'France',
     'apods:locales': 'en',
-    'apods:providedBy': 'Assemblée Virtuelle'
+    'apods:providedBy': 'Reconnexion'
   }
 ];
